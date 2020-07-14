f90e33dd323c759b51f115583000bfb6801bd2be
Mason <jeailfy9520@outlook.com>
2020-07-15 03:38:48 +0800

    feat: use router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Login';
 import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
