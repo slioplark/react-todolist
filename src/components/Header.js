54bdd82a5515fb98f0931f5aeee48709f9667e37
Mason <jeailfy9520@outlook.com>
2020-07-15 03:43:36 +0800

    feat: use Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,21 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Box display="flex" justifyContent="space-between" width="100%">
-          <Typography variant="h6">
-            React Todolist
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to="/">
+            <Typography variant="h6">
+              React Todolist
+            </Typography>
+          </Link>
+          <Link to="/login">
+            <Button color="inherit">Login</Button>
+          </Link>
         </Box>
       </Toolbar>
     </AppBar>
