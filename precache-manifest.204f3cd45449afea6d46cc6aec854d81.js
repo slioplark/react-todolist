50a614c8576e265651fd002c5eaea482d76cfd22
slioplark <31558373+slioplark@users.noreply.github.com>
2020-07-17 07:22:38 +0000

    Deploying to gh-pages from  @ 1fa623c75e1398f5f400f6534362cf6a8758a491 🚀

---
+++ precache-manifest.204f3cd45449afea6d46cc6aec854d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f577e820d57f81ca6e1116634671d40c",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "5cab67702a3d65460f25",
+    "url": "/react-todolist/static/css/main.6204e132.chunk.css"
+  },
+  {
+    "revision": "8b95b782af49c80d1832",
+    "url": "/react-todolist/static/js/2.cfaeed39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todolist/static/js/2.cfaeed39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cab67702a3d65460f25",
+    "url": "/react-todolist/static/js/main.37a61a79.chunk.js"
+  },
+  {
+    "revision": "c8e9edcc0b7d98c328a3",
+    "url": "/react-todolist/static/js/runtime-main.5ff039be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todolist/precache-manifest.4ebc38c0e13dda90dca75a6f5fa42378.js"
+  "/react-todolist/precache-manifest.204f3cd45449afea6d46cc6aec854d81.js"
 );
 
 self.addEventListener('message', (event) => {
